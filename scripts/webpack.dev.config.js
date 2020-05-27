5fe7ae27db4cf31d8097b0b952e773b5abc69bdc
nibilin <819231957@qq.com>
2020-05-27 21:21:35 +0800

    修复依赖漏洞

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -1,39 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const webpackConfigBase = require("./webpack.base.config");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const path = require('path')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const webpackConfigBase = require('./webpack.base.config')
-const OpenBrowserPlugin = require('open-browser-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const PORT = 8888
+const PORT = 8888;
 function resolve(relatedPath) {
-  return path.join(__dirname, relatedPath)
+  return path.join(__dirname, relatedPath);
 }
 const webpackConfigDev = {
   plugins: [
     // 定义环境变量为开发环境
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      "process.env.NODE_ENV": JSON.stringify("development"),
       IS_DEVELOPMETN: true,
     }),
-    // 将打包后的资源注入到html文件内    
+    // 将打包后的资源注入到html文件内
     new HtmlWebpackPlugin({
-      template: resolve('../app/index.html'),
-      mapConfig:'http://41.196.99.30/tgram-pgisbase/config/qdkjdsj_map_config.js'
+      template: resolve("../app/index.html"),
     }),
-    // new OpenBrowserPlugin({
-    //   url: `http://localhost:${PORT}/#/login`,
-    // }),
   ],
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    contentBase: resolve('../app'),
+    contentBase: resolve("../app"),
     historyApiFallback: false,
     hot: false,
-    host: '0.0.0.0',
+    host: "127.0.0.1",
     port: PORT,
   },
-}
+};
 
-module.exports = merge(webpackConfigBase, webpackConfigDev)
+module.exports = merge(webpackConfigBase, webpackConfigDev);
